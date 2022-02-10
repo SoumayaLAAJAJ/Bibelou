3ff4f0b853f1a2856a99ec8fd497d72bf807bb41
Soumaya Laajaj <soumaya.laajaj@hotmail.fr>
2022-02-10 19:34:58 +0100

    3eme jet

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+
+class UserFixtures extends Fixture
+{
+    // PROPRIETES
+    private $encoder;
+
+    // CONSTRUCTEURS
+    public function __construct(UserPasswordHasherInterface $userPasswordHasherInterface)
+    {
+        $this->encoder = $userPasswordHasherInterface;
+    }
+
+    // METHODES
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setFirstname("Soum");
+        $user->setName("Ljj");
+        $user->setEmail("souyaya19@hotmail.fr");
+        $user->setRoles(["ROLE_USER", "ROLE_ADMIN"]);
+        $password = $this->encoder->hashPassword($user, "admin");
+        $user->setPassword($password);
+        $manager->persist($user);
+
+        // 
+
+        $user = new User();
+        $user->setFirstname("Jean");
+        $user->setName("Neymar");
+        $user->setEmail("test@test.fr");
+        $user->setRoles(["ROLE_USER"]);
+        $password = $this->encoder->hashPassword($user, "admin");
+        $user->setPassword($password);
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
